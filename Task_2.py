ddaf51a4de575b57996475d47b1e7706466df373
vivimila <violamilashevskaya@yandex.by>
2022-10-27 11:46:35 +0300

    task 2 is solved

--- Task_2.py
+++ Task_2.py
@@ -1,3 +1,33 @@
 #Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 #Входные и выходные данные хранятся в отдельных текстовых файлах.
 
+givenText = 'AAAAAAAAAAAAABBBCAACCCCCCCCCC'
+print(f"Заданный текст: {givenText}")
+
+def coding(txt):
+    count = 1
+    res = ''
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
+
+print(f"Результат сжатия данных: {coding(givenText)}")
+
+def decoding(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+print(f"Результат возвращения исходных данных: {decoding(coding(givenText))}")
